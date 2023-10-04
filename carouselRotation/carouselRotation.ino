2c0bbf638c15daeb9cd47672b320180f6a914bd9
Dave Stewart <davs2rt@gmail.com>
2023-10-04 15:35:34 -0700

    more comments but untested carouselRotation

--- carouselRotation/carouselRotation.ino
+++ carouselRotation/carouselRotation.ino
@@ -1,23 +1,31 @@
 //author Ardeshir Lindstrom (Adi)
 // 2023-10-01
-// deployed to arduino on lead float.  works.
 
 const int stepper = 2; // pin number
 const int steps = 400;  // per 1 rotation
 const float cycle = 1/16000000;  // cycle time in secs
 const int startup = 15;
-
-double rpm = 0;
+double rpm = 1.5;
 
 void setup() {
-pinMode(2, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
+
 void loop() {
+  step(rpm); 
+}
+
+
+void step(double rpm){
+
+  int Del = (rpm*steps/60);/*steps per second*/
+  Del = 1000/Del;/*miliseconds per step*/
+  Del = Del/2; /*split the difference because the time occurs twice in the loop*/
  
-      digitalWrite(stepper, HIGH);
-      delay(53);
-      digitalWrite(stepper, LOW);
-      delay(53);
-   
+  digitalWrite(stepper, HIGH);
+  delay(Del);
+  digitalWrite(stepper, LOW);
+  delay(Del);
+ 
 }
